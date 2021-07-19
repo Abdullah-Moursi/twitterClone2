559645b7219813c65fcd6a1674246921db98b6e2
Abdullah-Moursi <moursiabdullah@gmail.com>
2021-07-19 12:15:46 +0200

    master

---
+++ src/components/TrendsKeyword.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TrendsKeyword = () => {
+    return (
+      <div className="keywords">
+        <div className="key">
+          <div className="keyword__heading">
+            <h4>Trends for you</h4>
+          </div>
+          <div className="country">Trend in USA</div>
+          <div className="keyword__name">
+            <strong>ISPR</strong>
+          </div>
+          <div className="keyword__tweets">2345K</div>
+        </div>
+      </div>
+    );
+}
+
+export default TrendsKeyword
